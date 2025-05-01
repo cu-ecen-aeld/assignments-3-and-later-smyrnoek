1d966547917aafc0ae6da19a8daff095b32bd988
Oleksandr Smyrnov <smyrnoek@gmail.com>
2025-05-02 00:56:39 +0200

    Unit test work

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -1,5 +1,4 @@
 #include "threading.h"
-#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -10,10 +9,28 @@
 
 void* threadfunc(void* thread_param)
 {
-
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    usleep(thread_func_args->wait_to_obtain_ms*1000);
+
+    int rc = pthread_mutex_lock(thread_func_args->mutex);
+    if (rc != 0) {
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    usleep(thread_func_args->wait_to_release_ms*1000);
+
+    rc = pthread_mutex_unlock(thread_func_args->mutex);
+    if (rc != 0) {
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +45,30 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+     struct thread_data *data = (struct thread_data *)malloc(sizeof(struct thread_data));
+     if (data == NULL) {
+        return false; // Failed to allocate memory
+     }
+     data->mutex = mutex;
+     data->thread_complete_success = false;
+     data->wait_to_obtain_ms = wait_to_obtain_ms;
+     data->wait_to_release_ms = wait_to_release_ms;
+
+     /* int rc = pthread_mutex_init(&data->mutex, NULL);
+     if (rc != 0) {
+         return false;
+     } */
+
+     // "data" is a pointer to a struct thread_data and is 
+     // explicitly cast to a void* (which is the expected argument 
+     // type for pthread_create).
+     int rc = pthread_create(thread, NULL, threadfunc, (void *)data);
+     if (rc != 0) {
+        free(data); // Free allocated memory on failure
+        return false;
+     }
+     
+     return true;
 }
 
